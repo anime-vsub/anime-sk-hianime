d6202411f107e16e11557030dcff7b7dfe58016a
Tachibana Shin <tachibshin@duck.com>
2025-04-07 09:46:39 +0000

    fix: standardize episode name spacing

--- logic/get-list-episodes.ts
+++ logic/get-list-episodes.ts
@@ -1,7 +1,9 @@
 import { load$ } from "./load-$.ts"
 
 export async function getListEpisodes(anime_id: string) {
-  const $ = await load$(`https://hianime.to/ajax/v2/episode/list/${anime_id.match(/\d+$/)![0]}`)
+  const $ = await load$(
+    `https://hianime.to/ajax/v2/episode/list/${anime_id.match(/\d+$/)![0]}`
+  )
 
   return $("a.ep-item")
     .toArray()
@@ -10,7 +12,7 @@ export async function getListEpisodes(anime_id: string) {
 
       const id = $a.attr("data-id")
       const order = $a.attr("data-number")
-      const name = $a.text().trim()
+      const name = $a.text().trim().replace(/\s+/g, " ")
       const title = $a.attr("title")
 
       if (id === undefined || order === undefined) return null
